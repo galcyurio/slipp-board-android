8ee3f4139e2af714841a800172f36f187772a4ae
galcyurio <galcyurio@gmail.com>
2018-08-05 23:58:00 +0900

    re #5 BoardItemAdapter 추가

---
+++ app/src/main/java/net/slipp/slippboard/misc/BoardItemAdapter.kt
@@ -0,0 +1,31 @@
+package net.slipp.slippboard.misc
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.item_board.view.*
+import net.slipp.slippboard.R
+import net.slipp.slippboard.data.Board
+
+class BoardItemAdapter : RecyclerView.Adapter<BoardItemAdapter.ViewHolder>() {
+    private val boards = mutableListOf<Board>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_board, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val board = boards[position]
+        with(holder.itemView) {
+            title.text = board.title
+
+            /* TODO: Board 데이터 바인딩 */
+        }
+    }
+
+    override fun getItemCount(): Int = boards.size
+
+    class ViewHolder(v: View) : RecyclerView.ViewHolder(v)
+}
