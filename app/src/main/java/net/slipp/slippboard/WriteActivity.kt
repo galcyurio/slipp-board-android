17a0ac5fef5570be49a4e1a1cc3265c7e2d72087
galcyurio <galcyurio@gmail.com>
2018-08-18 01:56:52 +0900

    re #21 WriteActivity 추가

---
+++ app/src/main/java/net/slipp/slippboard/WriteActivity.kt
@@ -0,0 +1,12 @@
+package net.slipp.slippboard
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class WriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}
