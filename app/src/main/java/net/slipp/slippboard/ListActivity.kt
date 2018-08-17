28e22d6ef2508710f2fb33732cdd5d6a720ed4c8
galcyurio <galcyurio@gmail.com>
2018-08-17 23:20:39 +0900

    re #14 Board 데이터 클래스 삭제

--- app/src/main/java/net/slipp/slippboard/ListActivity.kt
+++ app/src/main/java/net/slipp/slippboard/ListActivity.kt
@@ -3,20 +3,15 @@ package net.slipp.slippboard
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_list.*
-import net.slipp.slippboard.data.Board
-import net.slipp.slippboard.misc.BoardItemAdapter
+import net.slipp.slippboard.misc.PostItemAdapter
 
 class ListActivity : AppCompatActivity() {
-    private val adapter by lazy { BoardItemAdapter() }
+    private val adapter by lazy { PostItemAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
         recycler.adapter = adapter
-
-        /* TODO: 더미 데이터 제거할 것 */
-        adapter.add((1..30)
-            .map { Board("dummy title $it", "dummy content $it") })
     }
 }
